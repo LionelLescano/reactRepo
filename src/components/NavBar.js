1ae76a9ccecf807676ed23f2bafe67f627c75c56
Lionel Lescano <lionel.lescano98@gmail.com>
2022-09-06 22:40:53 -0300

    imagen solucionado

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -20,10 +20,10 @@ const NavBar = () => {
       </label>
       <div tabIndex="0" className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
         <div className="card-body">
-          <span className="font-bold text-lg">8 Items</span>
-          <span className="text-info">Subtotal: $999</span>
+          <span className="font-bold text-lg">Cantidad items</span>
+          <span className="text-info">Subtotal: $9999</span>
           <div className="card-actions">
-            <button className="btn btn-primary btn-block">View cart</button>
+            <button className="btn btn-primary btn-block">Carrito</button>
           </div>
         </div>
       </div>
--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,10 +1,10 @@
 
-const Producto = ( { title, price, image,description} ) => {
+const Producto = ( { title, price, img,description} ) => {
     return (
       
       <div className="card w-96 bg-base-100 shadow-xl">
       <figure className="px-10 pt-10">
-        <img src={image} alt="game" className="rounded-xl" />
+        <img src={img} alt="game" className="rounded-xl" />
       </figure>
       <div className="card-body items-center text-center">
         <h2 className="card-title">{title}</h2>
